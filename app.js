94b2b43c84c803d527febb8e0f2655c0743e6c78
joncancode <jc.haines19@gmail.com>
2017-06-18 17:51:55 -0400

    finished product

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ let appState = {
 };
 
 
-// const dummyItem = {
-//   name: "",
-//   checked: boolean
-// }
-
-
 // 2 make modification functions
   //add item
   let addItem = function(appState, text) {
@@ -17,41 +11,43 @@ let appState = {
       name: text,
       checked: false
     });
+    renderList(appState, $('.shopping-list'));
   }
 
   //delete item
   let deleteItem = function(appState, index) {
     appState.items.splice(index, 1);
+    renderList(appState, $('.shopping-list'));
   }
 
   // check(line-through) item
   let checkItem = function(appState, index) {
     appState.items[index].checked = !appState.items[index].checked;
-
+    renderList(appState, $('.shopping-list'));
   }
 
 // 3 make render functions
   //render for add
   let renderList = function (appState, element){
     let itemsHTML = appState.items.map(function(item, i){
-
-        return (`
-          <li data-item-index="${i}">
-            <span class="shopping-item ${item.checked ? 'shopping-item__checked': ''}"> ${item.name} </span>
-            <div class="shopping-item-controls">
-              <button class="shopping-item-toggle">
-                <span class="button-label">check</span>
-              </button>
-              <button class="shopping-item-delete">
-                <span class="button-label">delete</span>
-              </button>
-            </div>
-          </li>
-          `);
-
-  })
-      element.html(itemsHTML);
-      };
+      let checkedClass =  item.checked ? 'shopping-item__checked': '';
+      return (
+        `<li data-item-index="${i}">
+          <span class="shopping-item ${checkedClass}">${item.name}
+          </span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`
+        );
+      })
+    element.html(itemsHTML);
+    };
 
 
 // 4 make event listener function
@@ -62,31 +58,18 @@ $(document).ready(function() {
   $('#js-shopping-list-form').submit(function(event){
     event.preventDefault();
     addItem(appState, $('#shopping-list-entry').val());
-
-    renderList(appState, $('.shopping-list'));
+    // TODO: reset the input to empty
   });
 
-  //el for delete container's
-  $('.container').on('click', '.shopping-item-delete', function(event){
-    console.log("item got deleted")
-    const itemIndex = $(event.currentTarget).find('li').data('item-index');
-    console.log(event.currentTarget);
-      // .parent()
-      // .siblings('.shopping-item')
-      // .text()
-
-    deleteItem(appState, itemIndex);
-    renderList(appState, $('.shopping-list'));
+  // listen for click of delete button
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event){
+    const itemIndex = $(event.currentTarget).closest('li').data('item-index');
+    deleteItem(appState, itemIndex);  
   });
 
-  //el for check
-$('.container').on('click', '.shopping-item-toggle', function(event){
-    console.log($(event.currentTarget).find('li').data('item-index'));
-    const itemIndex = $(event.currentTarget).find('li').data('item-index');
-    console.log(itemIndex);
+  // listen for click of check button
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+    const itemIndex = $(event.currentTarget).closest('li').data('item-index');
     checkItem(appState, itemIndex);
-    renderList(appState, $('.shopping-list'));
-
   });
-
 });
